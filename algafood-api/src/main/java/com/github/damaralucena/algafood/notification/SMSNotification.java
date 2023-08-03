2e0e4cfebb0ff2c7c0979f2d998fa8d23020cce2
Damara Santos <63923428+DamaraLucena@users.noreply.github.com>
2023-08-03 11:18:20 -0300

    Bean disambiguation with @Qualifier

--- algafood-api/src/main/java/com/github/damaralucena/algafood/notification/SMSNotification.java
+++ algafood-api/src/main/java/com/github/damaralucena/algafood/notification/SMSNotification.java
@@ -1,5 +1,6 @@
 package com.github.damaralucena.algafood.notification;
 
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 import com.github.damaralucena.algafood.model.Client;
 
@@ -7,6 +8,7 @@ import com.github.damaralucena.algafood.model.Client;
 @Component
 public class SMSNotification implements Notifier {
 	
+	@Qualifier("SMS")
 	@Override
 	public void notification(Client client, String message) {
 		System.out.printf("Notifying %s by SMS through the phone %s: %s\n",
